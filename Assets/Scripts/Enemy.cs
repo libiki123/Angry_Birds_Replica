826b643fca09507f62869374380e2363f0e6fd78
unknown <bangatruong@gmail.com>
2021-05-07 21:06:22 -0400

    First commit
    - replicate Angry Bird's slingshot
    - simple collision calculation to check how fast enemy fall

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public float health = 4f;
+	public GameObject deadEffect;
+
+	private void OnCollisionEnter2D(Collision2D collInfo)
+	{
+		if (collInfo.relativeVelocity.magnitude > health)        // RelativeVelocity compare speed of 1 object to another in x & y
+		{                                                       // magnitude then turn this x & y into a single number
+			Die();
+		}
+	}
+
+	private void Die()
+	{
+		Instantiate(deadEffect, transform.position, Quaternion.identity);
+
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/SlingShot.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour
+{
+    public LineRenderer[] lineRenderers;
+    public Transform[] stripPositions;
+    public Transform center;
+    public Transform idlePos;
+    public GameObject birdPrefab;
+
+    public float maxLength;
+    public float bottomBoundary;
+    public float birdPosOffset;
+    public float force;
+
+    private Rigidbody2D birdRb;
+    private Collider2D birdColl;
+
+    private Vector3 currentPos;
+
+    private bool isMouseDown;
+
+    void Start()
+    {
+        CreateBird();
+        lineRenderers[0].positionCount = 2;
+        lineRenderers[1].positionCount = 2;
+        lineRenderers[0].SetPosition(0, stripPositions[0].position);
+        lineRenderers[1].SetPosition(0, stripPositions[1].position);
+    }
+
+    
+    void Update()
+    {
+		if (isMouseDown)
+		{
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10;
+            currentPos = Camera.main.ScreenToWorldPoint(mousePos);
+            currentPos = center.position + Vector3.ClampMagnitude(currentPos - center.position, maxLength);         // clamp the max length slingshot can screcth
+            currentPos = ClampBoundary(currentPos);
+
+            SetStrips(currentPos);
+
+			if (birdColl)
+			{
+                birdColl.enabled = true;
+			}
+		}
+		else
+		{
+            ResetStrips();
+		}
+    }
+
+    private void CreateBird()
+	{
+        birdRb = Instantiate(birdPrefab).GetComponent<Rigidbody2D>();
+        birdColl = birdRb.GetComponent<Collider2D>();
+        birdColl.enabled = false;                                           // stop bird from collide with the slingshot
+        
+    }
+
+	private Vector3 ClampBoundary(Vector3 vector)
+	{
+        vector.y = Mathf.Clamp(vector.y, bottomBoundary, 1000);
+        return vector;
+	}
+
+	private void OnMouseDown()
+	{
+        isMouseDown = true;
+        birdRb.isKinematic = true;
+    }
+
+	private void OnMouseUp()
+	{
+        isMouseDown = false;
+        birdRb.isKinematic = false;
+        Shoot();
+        
+	}
+
+	private void Shoot()
+	{
+        
+        Vector3 birdForce = (currentPos - center.position) * force * -1;
+        birdRb.velocity = birdForce;
+
+        birdRb = null;
+        birdColl = null;
+        Invoke("CreateBird", 2);
+
+	}
+
+	void ResetStrips()
+	{
+        currentPos = Vector3.Slerp(currentPos,idlePos.position,50f * Time.deltaTime);
+        SetStrips(currentPos);
+	}
+
+    void SetStrips(Vector3 pos)
+	{
+        lineRenderers[0].SetPosition(1, pos);
+        lineRenderers[1].SetPosition(1, pos);
+
+		if (birdRb)
+		{
+            Vector3 dir = pos - center.position;
+            birdRb.transform.position = pos + dir.normalized * birdPosOffset;
+            birdRb.transform.right = -dir.normalized;
+        }
+	}
+}
